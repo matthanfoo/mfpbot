0d47b8df2dc61b4bdbf2a46479d7e1cb2d049f31
matthanfoo <70493676+matthanfoo@users.noreply.github.com>
2023-01-08 10:18:55 +0800

    Add files via upload

--- tpfpostgre.py
+++ tpfpostgre.py
@@ -28,37 +28,37 @@ def execute_pgsql(sql: str, values=[], fetchall=False, fetchone=False, fetchmany
         if fetchall:
             if len(values)==0:
                 print('fetchall, lv=0')
-                resultpre = cur.execute(sql)
-                print(resultpre)
-                if resultpre:
-                    result = resultpre.fetchall()
+                result = cur.execute(sql).fetchall()
+##                print(resultpre)
+##                if resultpre:
+##                    result = resultpre.fetchall()
             else:
                 print('fetchall, lv=+')
-                resultpre = cur.execute(sql, values)
-                if resultpre:
-                    result = resultpre.fetchall()
+                result = cur.execute(sql, values).fetchall()
+##                if resultpre:
+##                    result = resultpre.fetchall()
         elif fetchone: 
             if len(values)==0:
                 print('fetchone, lv=0')
-                resultpre = cur.execute(sql)
-                if resultpre:
-                    result = resultpre.fetchone()
+                resultpre = cur.execute(sql).fetchone()
+##                if resultpre:
+##                    result = resultpre.fetchone()
             else:
                 print('fetchone, lv=+')
-                resultpre = cur.execute(sql, values)
-                if resultpre:
-                    result = resultpre.fetchone()
+                result = cur.execute(sql, values)
+##                if resultpre:
+##                    result = resultpre.fetchone()
         elif fetchmany:
             if len(values)==0:
                 print('fetchmany, lv=0')
-                resultpre = cur.execute(sql)
-                if resultpre:
-                    result = resultpre.fetchmany()
+                resultpre = cur.execute(sql).fetchmany()
+##                if resultpre:
+##                    result = resultpre.fetchmany()
             else:
                 print('fetchmany, lv=+')
-                resultpre = cur.execute(sql, values)
-                if resultpre:
-                    result = resultpre.fetchmany()
+                result = cur.execute(sql, values)
+##                if resultpre:
+##                    result = resultpre.fetchmany()
         else:
             if len(values)==0:
                 print('nofetchall, lv=0')
